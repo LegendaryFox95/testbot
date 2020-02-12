a7a23a27418472d72693663ccf98c4c6ea9ebd4b
LegendaryFox95 <60966075+LegendaryFox95@users.noreply.github.com>
2020-02-12 15:04:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ client.on('message', msg => {
 
 client.on('message', msg => {
   if (msg.content.endsWith('!blink')) {
-    msg.channel.send(`<:mfwgirldoesntsuckmypeesos:676056848092233774>`);
+    msg.channel.send(`<a:mfwgirldoesntsuckmypeesos:676056848092233774>`);
   }
 });
 
